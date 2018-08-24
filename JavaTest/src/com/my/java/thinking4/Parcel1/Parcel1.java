78f774d92146b6b3928b78c4f5c94395cb01ea3c
tangPlayGithub <441711533@qq.com>
2018-08-25 01:42:31 +0800

    444444

--- JavaTest/src/com/my/java/thinking4/Parcel1/Parcel1.java
+++ JavaTest/src/com/my/java/thinking4/Parcel1/Parcel1.java
@@ -1,6 +1,12 @@
 package com.my.java.thinking4.Parcel1;
 
 public class Parcel1 {
-
-
+	public void test() {
+		
+	}
+	
+    public static void main(String[] args) {
+		System.out.println("aaaaaa");
+	}
+    
 }
