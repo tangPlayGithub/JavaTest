68535d0a7364c9422f6dcbde711a993cf4389ad4
tangPlayGithub <441711533@qq.com>
2018-08-30 00:43:06 +0800

    java算法---顺序表结构、链表结构

---
+++ JavaTest/src/com/my/java/algorithm/P2_2.java
@@ -0,0 +1,16 @@
+package com.my.java.algorithm;
+
+class DATA2 {
+	String key; // 结点的关键字
+	String name;
+	int age;
+}
+
+class CLType{
+	DATA2 nodeData=new DATA2();
+	CLType nextNode;
+}
+
+public class P2_2 {
+
+}
